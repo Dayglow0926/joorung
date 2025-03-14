1cc6c341675e850ae9f5dc70149e90d3000a0daa
Dayglow0926 <dayglow0926@gmail.com>
2025-03-14 16:34:38 +0900

    ✨: mdParser 추가 - gray-matter 를 사용해서 .md 파일 처리

---
+++ app/utils/mdParser.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+export const mdParser = (slug: string) => {
+  const filePath = path.join(
+    process.cwd(),
+    'contents',
+    '__posts',
+    `${slug}.md`
+  );
+  console.log(filePath);
+  if (!fs.existsSync(filePath)) {
+    return null;
+  }
+
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const { data, content } = matter(fileContent);
+
+  return { ...data, content };
+};
