266a249b84e03a6d08c41331afeb8c9815fb2ba0
Dayglow0926 <dayglow0926@gmail.com>
2025-03-14 18:06:55 +0900

    ✨: root 페이지 헤더 타이틀 링크 추가

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from '@remix-run/react';
+import { Link, Outlet } from '@remix-run/react';
 import ToggleTheme from './common/ToggleTheme';
 
 export default function Layout() {
   return (
     <div className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white">
       <nav className="p-4 flex justify-between items-center border-b border-gray-300 dark:border-gray-700">
-        <h1 className="text-2xl font-bold">📝 주렁 블로그</h1>
+        <Link to={'/'}>
+          <h1 className="text-2xl font-bold">📝 주렁 블로그</h1>
+        </Link>
         <ToggleTheme />
       </nav>
       <main className="max-w-3xl mx-auto p-6">
